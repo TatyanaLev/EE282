296a9c2aca4ab3d80f5db15a8a915726b900d84d
Tatyana Lev <37021262+TatyanaLev@users.noreply.github.com>
2020-12-15 21:11:27 -0800

    Update depth.sh

--- code/scripts/depth.sh
+++ code/scripts/depth.sh
@@ -1,4 +1,6 @@
-# a depthFile.txt is generated from running bioinfokit (python) on raw FASTQ files
+# a depthFile.txt is generated from running bioinfokit package (python script calculate_depth.py) on raw FASTQ files
 # resulting depths.txt will be one column with as many depth entries as supplied FASTQ files and in the same order
+
+python3 calculate_depth.py > depthsFile.txt
 
 gawk '{print $6}' depthsFile.txt > depths.txt
