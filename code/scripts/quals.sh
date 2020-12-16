dca4b56147c3bedbc595b9481fdf839749f6fbc9
Tatyana Lev <37021262+TatyanaLev@users.noreply.github.com>
2020-12-15 21:19:06 -0800

    Update quals.sh

--- code/scripts/quals.sh
+++ code/scripts/quals.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # average Phred quality is extracted from a stats file which is generated with samtools stats while aligning FASTQs into BAM
 # resulting quals.txt consists of one column with as many quality entries as supplied stats files and in the same order
 
