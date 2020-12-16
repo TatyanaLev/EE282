60eaccf0c41e8d2cedf05fe0cf5d5b63e9a6e1ca
Tatyana Lev <37021262+TatyanaLev@users.noreply.github.com>
2020-12-15 21:15:34 -0800

    Create breadth.sh

---
+++ code/scripts/breadth.sh
@@ -0,0 +1,21 @@
+# this script takes in a list of sample names in the s1_list.txt 
+# and a list of VCFs generated with GATK's DiagnoseTargets
+
+wkdir='/dfs4/som/tzhuravl/NavinRepr/'
+brd='Breadth/'
+Npass='Npasses.txt'
+
+#this file contains aligned BAMs
+f=/dfs4/som/tzhuravl/NavinRepr/s1_list.txt
+
+while IFS="" read -r f || [ -n "$f" ]
+do
+	for i in $f; 
+		do
+	tail -n +474 $wkdir$brd$i$textext | awk '{print $7}' | grep "PASS" | wc -l >> $wkdir$brd$Npass
+		done
+done < /dfs4/som/tzhuravl/NavinRepr/s1_list.txt
+
+cat intervals.list | wc -l > totalIntervals.txt
+
+#then in R, do (Npass/totalIntervals)*100
